2bb0866b1b874f297e8130eafe35ac678c6d3ca6
dyh <yzcczdyh@gmail.com>
2017-09-07 21:00:50 +0800

    phpstorm config

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
