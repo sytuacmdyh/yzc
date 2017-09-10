e1a8cb236da39bb0c80e3fdfc4b38e2b56e71ce3
dyh <yzcczdyh@gmail.com>
2017-09-10 22:41:48 +0800

    add horizon auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Horizon\Horizon;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+        Horizon::auth(function ($request) {
+             return true;
+        });
     }
 
     /**
