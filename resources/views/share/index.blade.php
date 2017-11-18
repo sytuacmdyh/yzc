1812510dd806ec496c448df7f8e4029763ec8f70
dyh <yzcczdyh@gmail.com>
2017-11-18 19:29:01 +0800

    bug-fix

--- resources/views/share/index.blade.php
+++ resources/views/share/index.blade.php
@@ -35,13 +35,13 @@
                 <div class="panel-body">
                     <ul class="nav nav-tabs">
                         @for ($i = 0;$i<10;$i++)
-                            <li class="{{$i==0?'active':''}}"><a data-toggle="tab" href="#tab{{$i}}">TAB{{$i}}</a></li>
+                            <li class="{{$i==0?'active':''}}"><a data-toggle="tab" href="#tabPublic{{$i}}">TAB{{$i}}</a></li>
                         @endfor
                     </ul>
 
                     <div class="tab-content">
                         @for ($i = 0;$i<10;$i++)
-                            <div id="tab{{$i}}" class="tab-pane fade{{$i==0?' in active':''}}">
+                            <div id="tabPublic{{$i}}" class="tab-pane fade{{$i==0?' in active':''}}">
                                 <pre>
                                     <code class="">{{htmlspecialchars(@$sharesPublic[$i]['data'])}}</code>
                                 </pre>
