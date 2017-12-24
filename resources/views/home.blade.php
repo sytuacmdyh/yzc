a3985d0c287e567cfe7f8082141a543e75295df0
dyh <yzcczdyh@gmail.com>
2017-12-24 11:34:21 +0800

    增加jupyter notebook入口

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <ol class="breadcrumb" style="background-color: #fff">
-            <li><a href="#">Home</a></li>
-        </ol>
-    </div>
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Home
-                </div>
-                <div class="panel-body">
-                    <a href="{{route('notes.index')}}" class="btn btn-default btn-block">Note</a>
-                    <a href="{{route('shares.index')}}" class="btn btn-default btn-block">Share</a>
+    <div class="container">
+        <div class="row">
+            <ol class="breadcrumb" style="background-color: #fff">
+                <li><a href="#">Home</a></li>
+            </ol>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Home
+                    </div>
+                    <div class="panel-body">
+                        <a href="{{url('/notes')}}" class="btn btn-default btn-block">Note</a>
+                        <a href="{{url('/shares')}}" class="btn btn-default btn-block">Share</a>
+                        <a href="{{url('/notebook')}}" class="btn btn-default btn-block">Jupyter Notebook</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
