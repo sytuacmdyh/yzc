6aa67ca9ebec4dac76de4bc3edded3f1bb2014ce
丁英豪 <dingyinghao@dm-maker.com>
2018-12-04 21:09:09 +0800

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \URL::forceScheme('https');
         Schema::defaultStringLength(191);
         Horizon::auth(function ($request) {
              return true;
